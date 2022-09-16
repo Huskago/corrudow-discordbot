72bba02cd75a79504035a810bf8c857104609ab6
Huskago <tiagoflfl0202@gmail.com>
2022-09-16 16:27:33 +0200

    update status bot

--- events/client/ready.js
+++ events/client/ready.js
@@ -17,12 +17,12 @@ module.exports = {
         devGuild.commands.set(client.commands.map(command => command));
 
         const statuses = [
-            // { name: "de la musique avec les Shadow", type: ActivityType.Listening },
-            // { name: "à aider Huskago", type: ActivityType.Playing },
-            // { name: `la version ${process.env.npm_package_version}`, type: ActivityType.Watching },
-            // { name: "la salope de tiberius", type: ActivityType.Playing },
-            // { name: "Huskago, notre dieu à tous !", type: ActivityType.Watching },
-            // { name: "Huskago, notre dieu à tous !", type: ActivityType.Listening },
+            { name: "de la musique avec les Shadow", type: ActivityType.Listening },
+            { name: "à aider Huskago", type: ActivityType.Playing },
+            { name: `la version ${process.env.npm_package_version}`, type: ActivityType.Watching },
+            { name: "la salope de tiberius", type: ActivityType.Playing },
+            { name: "Huskago, notre dieu à tous !", type: ActivityType.Watching },
+            { name: "Huskago, notre dieu à tous !", type: ActivityType.Listening },
             { name: `${(client.guilds.cache.get("1000768975963164803").roles.cache.get("1000810183779438793").members.filter(member => !member.user.bot).size - 1).toFixed()} joueurs !`, type: ActivityType.Listening },
         ];
 
